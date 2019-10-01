bc628514f7a74b54573db729b74bd6eacb3bd2f6
PedroZak <32416399+PedroZak@users.noreply.github.com>
2019-10-01 11:02:03 -0300

    Working
    Classe Jogo e Main_logic funcionando.
    
    Exibindo tela.

---
+++ Main_logic.java
@@ -0,0 +1,21 @@
+//package Main_logic;
+//import java.awt.Window;
+import javax.swing.JFrame;
+
+public class Main 
+{ 
+	public static void main(String[] args)
+	{
+		Jogo jogo = new Jogo();
+		
+		JFrame janela = new JFrame("2048");
+		janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		janela.setResizable(false);
+		janela.add(jogo);
+		janela.pack();
+		janela.setLocationRelativeTo(null);
+		janela.setVisible(true);
+		
+		jogo.start();
+	}
+}
